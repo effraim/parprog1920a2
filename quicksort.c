68f6ffcba1c43914c49f5cadf73d0bf15bc3b4bc
effraim <32572199+effraim@users.noreply.github.com>
2020-05-29 23:16:05 +0300

    Update quicksort.c
    not finished

--- quicksort.c
+++ quicksort.c
@@ -1,13 +1,89 @@
-// code base for various sorting algorithms
-// compile with e.g. gcc -O2 -Wall sorting-base.c -o sorting-base
 
+// compile with e.g. gcc -O2 -Wall sorting-base.c -o sorting-base
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <string.h>
 
-
+#define THREAD_NUM 4
 #define N 1000000
-#define CUTOFF 10
+#define CREATE_LIMIT 10
+#define MESSAGES (N/OFF)
+
+// conditional variable(receiver waits on this)
+pthread_cond_t msg_in = PTHREAD_INITIALIZE;
+
+// conditional variable(sender waits on this)
+pthread_cond_t msg_out = PTHREAD_INITIALIZE;
+
+// mutex ελέγχει ποιό thread έχει access στην κοινή πληροφορία
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZE;
+
+int maxcounter = 0;
+
+
+typedef struct {
+	Info array[MESSAGES];
+  int flag;
+	int qin, qout;//για το msg_in msg_out
+	int count; //αριθμός μυνημάτων
+  int start;
+	int end;
+}BUFFER;
+
+BUFFER buffer;
+
+int main() {
+double *a;
+int i;
+counter = 0;
+
+  a = (double *)malloc(N*sizeof(double));
+  if (a==NULL) {
+    printf("error in malloc\n");
+    exit(1);
+  }
+
+  // fill array with random numbers
+  srand(time(NULL));
+  for (i=0;i<N;i++) {
+    a[i] = (double)rand()/RAND_MAX;
+  }
+
+  fill_buffer();
+  send_msg(buffer); // stelnoume to prwto message
+  pthread_t threads[THREADS_NUM]; 
+  
+  // create threads
+	for(i=0;i<THREADS_NUM;i++)
+		pthread_create(&threads[i],NULL,start_thread,NULL);
+
+  
+
+
+	for(i=0;i<THREADS;i++)
+		pthread_join(threads[i],NULL);
+ 
+
+  // check sorting
+  for (i=0;i<(N-1);i++) {
+    if (a[i]>a[i+1]) {
+      printf("Sort failed!\n");
+      break;
+    }
+  }  
+
+  free(a);
+  
+  	
+
+  pthread_mutex_destroy(&mutex);
+  free(a);
+  return 0;
+}
+
 
 void inssort(double *a,int n) {
 int i,j;
@@ -69,37 +145,4 @@ int i;
   quicksort(a,i);
   quicksort(a+i,n-i);
   
-}
-
-
-int main() {
-double *a;
-int i;
- 
-  a = (double *)malloc(N*sizeof(double));
-  if (a==NULL) {
-    printf("error in malloc\n");
-    exit(1);
-  }
-
-  // fill array with random numbers
-  srand(time(NULL));
-  for (i=0;i<N;i++) {
-    a[i] = (double)rand()/RAND_MAX;
-  }
-
-  // sort array
-  quicksort(a,N);
-
-  // check sorting
-  for (i=0;i<(N-1);i++) {
-    if (a[i]>a[i+1]) {
-      printf("Sort failed!\n");
-      break;
-    }
-  }  
-
-  free(a);
-  
-  return 0;
 }
